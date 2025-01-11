5eff87690b3a14baa8550255eacdea9f79cfd015
Isaac-chou-123 <3225293665@qq.com>
2025-01-11 09:54:21 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,25 +8,30 @@ export default defineUserConfig({
   blog: false,
   lang: 'zh-CN',
   head: [
-    // 设置 favor.ico，.vuepress/public 下
     [
+        'link', { rel: 'icon', href: '/images/solar--cat-linear.svg' },
         'link', { rel: 'icon', href: '/images/solar--cat-linear.svg' }
     ]
   ],
+  locales: {
+    '/': { lang: 'zh-CN', title: 'NcatBot 文档' }
+  },
   theme: plumeTheme({
     lang: 'zh-CN',
-    locales: {
-      '/': { lang: 'zh-CN', title: '文档' }
-    },
     blog: false,
     plot: true,
+    navbar: [
+      { text: '首页', link: '/' },
+      { text: '快速开始', link: '/guide/ucli0mqd/' },
+    ],
     sidebar: 'auto',
     notes,
     changelog: true,
     contributors: true,
     plugins: {
       git: process.env.NODE_ENV === 'production'
-    }
+    },
+    footer: { message: "", copyright: "© 2025 NcatBot" }
   }),
   bundler: viteBundler(),
   
